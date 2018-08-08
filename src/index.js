bbd47e509e9ae857107fb58c2bd7355515f04cbb
Danny Wong <wong.danny29@gmail.com>
2018-08-07 20:27:16 -0700

    react mdl lib added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+
+class Main extends React.Component {
+	render () {
+		return (
+			<div>
+				{/* Uses a transparent header that draws on top of the layout's background */}
+				<div style={{height: '300px', position: 'relative'}}>
+				    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
+				        <Header transparent title="Title" style={{color: 'white'}}>
+				            <Navigation>
+				                <a href="#">Link</a>
+				                <a href="#">Link</a>
+				                <a href="#">Link</a>
+				                <a href="#">Link</a>
+				            </Navigation>
+				        </Header>
+				        <Drawer title="Title">
+				            <Navigation>
+				                <a href="#">Link</a>
+				                <a href="#">Link</a>
+				                <a href="#">Link</a>
+				                <a href="#">Link</a>
+				            </Navigation>
+				        </Drawer>
+				        <Content />
+				    </Layout>
+				</div>
+
+			</div> 
+
+
+			);
+	}
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
