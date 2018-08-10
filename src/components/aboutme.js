9ddbdd77ae1e9ec57425ab541d3d036d64e62d34
Danny Wong <wong.danny29@gmail.com>
2018-08-09 22:41:05 -0700

    react mdl lib added

---
+++ src/components/aboutme.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'; 
+
+class About extends Component {
+	render() {
+
+		return (
+				<div> 
+
+					<h1> About page </h1>
+
+
+
+
+				</div>
+
+
+			); 
+
+	}
+}
+
+export default About; 
---
+++ src/components/landingpage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'; 
+
+class LandingPage extends Component {
+	render() {
+
+		return (
+				<div> 
+
+					<h1> LandingPage page </h1>
+
+
+
+
+				</div>
+
+
+			); 
+
+	}
+}
+
+export default LandingPage; 
