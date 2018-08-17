645aa59d368e594f35c088fda4c083547095e2fb
Danny Wong <wong.danny29@gmail.com>
2018-08-17 15:17:19 -0700

    clip page done

--- src/components/clip.js
+++ src/components/clip.js
@@ -19,27 +19,134 @@ class Clip extends Component {
 								<Cell col={12}>
 									<div className="fancy-font" > 
 									    <p style={{fontSize:'35px'}}> Clip </p>	 
-									    <p style={{fontSize:'20px', color:'blue'}}> THE VILLAIN </p>	 
+									    <p style={{fontSize:'20px', color:'blue'}}> THE VILLAIN </p>	
 
 									</div>
-									
+									<div className="box">
+									    
+
+									    <p> Yes, you can call me Big Daddy. I am Team Yuri's Developer and part of the Content Creations Team. I am good with animals, both real and balloon. </p>
+									    <p style={{fontSize:'18px', color:'gray'}}> Hold up, I need my pants off for this. <br /> <br />
+									    </p> 
+									</div>
+								</Cell>
+								
+							</Grid>
+				</div>	
+
+							{/* Games */}
+						<div style={{background:'black', padding:'50px'}}> 
+	
+							<Grid style={{margin:'20px'}}>
+
+								<Cell col={12}> 
+									<p className="fancy-font" style={{fontSize:'25px', color:'white'}}> LEAGUE OF LEGENDS: KILLER LADIES </p> 
+									<p className="box" style={{fontSize:'15px', color:'white'}}> As a mid-lane main, below are my top champions that I enjoy playing. I have been playing since Season 4. My playstyle is more towards burst AP assassins, especially with a gap closer or jump. In addition, I do like Top lane and Jungle.</p> 
+
+								</Cell>
+								<Cell col={12}> 
+
+								</Cell>
+								<Cell col={3}> 
+									<img style={{width: '100%', height:'90%' }} src={('https://pre00.deviantart.net/a0cb/th/pre/f/2015/238/1/d/screen_shot_2015_08_26_at_8_54_43_pm_by_frangorman-d977fux.png')} />
+									<p className="fancy-font" style={{fontSize:'20px', color:'white'}}> LeBLANC </p>
+								</Cell>
+								
+								
+								<Cell col={3}> 
+									<img style={{width: '100%', height:'90%' }} src={('https://pre00.deviantart.net/6026/th/pre/i/2016/045/d/8/diana_by_haeaswen-d9rrr1e.jpg')} />
+									<p className="fancy-font" style={{fontSize:'20px', color:'white'}}> DIANA </p>
 								</Cell>
 
 								
-						        <Cell col={12}>
 
-						        	<div className="box" > 
-									</div>
-										<SocialIcon url="https://www.youtube.com/channel/UCR7FyFJRDULUTpYFAWsxqVw?view_as=subscriber" network="youtube"/>
-										<SocialIcon url="https://www.instagram.com/_clipclopper/" network="instagram"/>
-										<SocialIcon url="https://www.twitch.tv/clipclopper" network="twitch"/> 
+								<Cell col={3}> 
+									<img style={{width: '100%', height:'90%' }} src={('http://www.lol-wallpapers.com/wp-content/uploads/2018/06/Irelia-by-Haeaswen-HD-Wallpaper-Background-Fan-Art-Artwork-League-of-Legends-lol.jpg')} />
+									<p className="fancy-font" style={{fontSize:'20px', color:'white'}}> IRELIA </p>
+								</Cell>
 
-						        </Cell>
+								<Cell col={3}> 
 
-						        <Cell col={7}> </Cell> 
-						        <Cell col={1}> </Cell>
-						    </Grid>
-					</div>
+									<img style={{width: '100%', height:'90%' }} src={('https://img00.deviantart.net/a1b9/i/2015/127/1/9/katarina_by_obywatelsowa-d8shm86.jpg')} />
+									<p className="fancy-font" style={{fontSize:'20px', color:'white'}}> KATARINA </p> 
+
+								</Cell>
+							{/* Dividing Line */}
+								<Cell col={12}> 
+								</Cell>
+							
+								<Cell col={3}> 
+									<img style={{width: '100%', height:'90%' }} src={('https://fanart.na.leagueoflegends.com/media/user_uploads/arts/20180405/s/5b073367-9e23-4a2d-815c-6e004e862db6_1000x1000_scaled.jpg')} />
+									<p className="fancy-font" style={{fontSize:'20px', color:'white'}}> AKALI </p>
+								</Cell>
+								<Cell col={3}> 
+									<img style={{width: '100%', height:'90%' }} src={('https://i.pinimg.com/originals/48/3b/38/483b38fc0c8c28b27b673bf84dc34a90.jpg')} />
+									<p className="fancy-font" style={{fontSize:'20px', color:'white'}}> AHRI </p>
+								</Cell>
+								<Cell col={3}> 
+									<img style={{width: '100%', height:'90%' }} src={('http://www.lol-wallpapers.com/wp-content/uploads/2017/04/Nidalee-Portrait-by-Haeaswen-HD-Wallpaper-Fan-Artwork-League-of-Legends-lol.jpg')} />
+									<p className="fancy-font" style={{fontSize:'20px', color:'white'}}> NIDALEE </p>
+								</Cell>
+								<Cell col={3}> 
+									<img style={{width: '100%', height:'90%' }} src={('https://orig00.deviantart.net/2355/f/2012/059/b/0/evelynn_by_nyra119-d4rally.jpg')} />
+									<p className="fancy-font" style={{fontSize:'20px', color:'white'}}> EVE </p>
+								</Cell>
+							</Grid>
+						</div>
+						<div style={{ padding:'50px'}}> 
+							<Grid> 
+								<Cell col={8}>
+									<img style={{width:'90%', height:'100%'}} src={('https://s33.postimg.cc/jzm2gvo7j/Screen_Shot_2018-08-17_at_2.28.59_PM.png')}/>
+								</Cell>
+								<Cell col={4} > 
+										<div className="box" > 
+										 <p style={{textAlign:'left', fontSize:'18px'}}>  Minecraft is one of those games that I end up spending many hours playing. I prefer the PvP, building, and adventuring aspect of the game. </p></div>
+								</Cell>
+								<Cell col={12}>  <p> </p></Cell>
+
+								<Cell col={4} > 
+										<div className="box" > 
+										 <p style={{textAlign:'right', fontSize:'18px'}}>  I am also a somewhat active GTA V player. I only play the GTA Online version. </p></div>
+								</Cell>
+								<Cell col={8}>
+									<img style={{width:'90%', height:'100%'}} src={('https://s33.postimg.cc/usfsk26gf/sds.png')}/>
+								</Cell>
+
+								<Cell col={12}>  <p> </p></Cell>
+								<Cell col={8}>
+									<img style={{width:'90%', height:'100%'}} src={('https://s33.postimg.cc/6l8zkwezj/Screen_Shot_2018-08-17_at_1.56.18_AM.png')}/>
+								</Cell>
+								<Cell col={4} > 
+										<div className="box" > 
+										 <p style={{textAlign:'left', fontSize:'18px'}}>  I have been playing Pokemon HeartGold recently. <br /> Here is my team. </p></div>
+								</Cell>
+								<Cell col={12}>  <p> </p></Cell>
+
+								<Cell col={4} > 
+										<div className="box" > 
+										 <p style={{textAlign:'right', fontSize:'18px'}}>  After the Johto region, I only took my Growlithe and Togetic to the Kanto region to continue my adventure. </p></div>
+								</Cell>
+								<Cell col={8}>
+									<img style={{width:'90%', height:'100%'}} src={('https://s33.postimg.cc/i05gpibwf/Screen_Shot_2018-08-17_at_1.56.35_AM.png')}/>
+								</Cell>
+
+							</Grid>
+							
+						</div>
+
+				<div style={{margin:'50px'}}> 
+					<Grid>
+						<Cell col={12}>
+						{/* social medias */}
+
+									<SocialIcon url="https://www.youtube.com/channel/UCR7FyFJRDULUTpYFAWsxqVw?view_as=subscriber" network="youtube"/>
+									<SocialIcon url="https://www.instagram.com/_clipclopper/" network="instagram"/>
+									<SocialIcon url="https://www.twitch.tv/clipclopper" network="twitch"/>
+						</Cell>
+					</Grid>
+				</div>
+
+							
 
 			</div>
 				); 
