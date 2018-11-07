2b65ae303e6f5640cca46d3ec788c671c7698b07
Danny Wong <wong.danny29@gmail.com>
2018-11-06 18:15:27 -0800

    changed gray text to white

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -50,7 +50,7 @@ class LandingPage extends Component {
 						        <img src={require('../images/yuri.png')} style={{width:'15%', align:'auto'}}/>
 						        <p><b>THE QUEEN</b></p>
 
-						        <p className="w3-text-grey" style={{textAlign:'left'}}>I am Yuri. I started streaming a few years ago and I came to realize that the community was a lot more toxic than it had to be. I always thought a little trolling is funny, friendly banters spice up conversations and a little sarcasm could be appreciated. That wasn’t what I experienced as a streamer. I’ve had people attack me for the way I look, the way I sound and people who legitimately would not stop harassing me. I came to realize that there is a part of this gaming universe I escape to that’s simply unsafe and unkind. That’s when I decided, I’m going to start my own community. One where people like myself could escape to and find solace without being afraid to show everyone who they really are. I brought together a group of very talented people to start a movement.</p>
+						        <p className="w3-text-white" style={{textAlign:'left'}}>I am Yuri. I started streaming a few years ago and I came to realize that the community was a lot more toxic than it had to be. I always thought a little trolling is funny, friendly banters spice up conversations and a little sarcasm could be appreciated. That wasn’t what I experienced as a streamer. I’ve had people attack me for the way I look, the way I sound and people who legitimately would not stop harassing me. I came to realize that there is a part of this gaming universe I escape to that’s simply unsafe and unkind. That’s when I decided, I’m going to start my own community. One where people like myself could escape to and find solace without being afraid to show everyone who they really are. I brought together a group of very talented people to start a movement.</p>
 						  </Tab>
 						  <Tab eventKey={2} title="CLIP" style={{color:'blue'}}>
 						  	    <p> </p>
@@ -60,7 +60,7 @@ class LandingPage extends Component {
 
 						        <p><b>THE VILLAIN</b></p>
 
-						        <p className="w3-text-grey" style={{textAlign:'left'}}>I go by the name Clip. I am an aspiring streamer and computer programmer. I like doing frontend development. This simple website was created all by myself. As a gay boy, I am extrememly supportive of all of Team Yuri values. I believe by creating a positive environment will provide an enriching atmosphere for the gaming community. Games that I enjoy include League of Legends, Minecraft, The Sims, WWE 2k18, and VR Chat. My life goals is to not only become a professional frontend developer but also become a role model to inspire others that are in similar situations. I grew up from a traditional household where it is looked down upon for my lifestyle; thus, this taught me many life lessons and made up the person who I am today. Stay tuned for more Team Yuri features.</p>
+						        <p className="w3-text-white" style={{textAlign:'left'}}>I go by the name Clip. I am an aspiring streamer and computer programmer. I like doing frontend development. This simple website was created all by myself. As a gay boy, I am extrememly supportive of all of Team Yuri values. I believe by creating a positive environment will provide an enriching atmosphere for the gaming community. Games that I enjoy include League of Legends, Minecraft, The Sims, WWE 2k18, and VR Chat. My life goals is to not only become a professional frontend developer but also become a role model to inspire others that are in similar situations. I grew up from a traditional household where it is looked down upon for my lifestyle; thus, this taught me many life lessons and made up the person who I am today. Stay tuned for more Team Yuri features.</p>
 						  </Tab>
 						  <Tab eventKey={3} title="MOMO" style={{color:'red'}}>
 								<p> </p>
@@ -70,7 +70,7 @@ class LandingPage extends Component {
 
 						        <p><b>THE SCIENTIST</b></p>
 
-						        <p className="w3-text-grey" style={{textAlign:'left'}}> Just a casual in all things. Play games with this dude called Trinimmortal sometimes.</p>
+						        <p className="w3-text-white" style={{textAlign:'left'}}> Just a casual in all things. Play games with this dude called Trinimmortal sometimes.</p>
 						  </Tab>
 						  <Tab eventKey={4} title="SUGAR" style={{color:'orange'}}>
 								<p> </p>
@@ -80,7 +80,7 @@ class LandingPage extends Component {
 
 						        <p><b>THE WITCH</b></p>
 
-						        <p className="w3-text-grey" style={{textAlign:'left'}}> Just a casual in all things. Play games with this dude called Trinimmortal sometimes.</p>
+						        <p className="w3-text-white" style={{textAlign:'left'}}> Just a casual in all things. Play games with this dude called Trinimmortal sometimes.</p>
 						  </Tab>
 						  <Tab eventKey={5} title="JAYDEN" style={{color:'white'}}>
 								<p> </p>
@@ -90,7 +90,7 @@ class LandingPage extends Component {
 
 						        <p><b>THE MUSE</b></p>
 
-						        <p className="w3-text-grey" style={{textAlign:'left'}}> Just a casual in all things. Play games with this dude called Trinimmortal sometimes.</p>
+						        <p className="w3-text-white" style={{textAlign:'left'}}> Just a casual in all things. Play games with this dude called Trinimmortal sometimes.</p>
 						  </Tab>
 						  <Tab eventKey={6} title="EDI" style={{color:'yellow'}}>
 								<p> </p>
@@ -100,7 +100,7 @@ class LandingPage extends Component {
 
 						        <p><b>THE VIRTUOSO</b></p>
 
-						        <p className="w3-text-grey" style={{textAlign:'left'}}> Artist | ENG/日本語 OK! Owner of Give Me Space.</p>
+						        <p className="w3-text-white" style={{textAlign:'left'}}> Artist | ENG/日本語 OK! Owner of Give Me Space.</p>
 						  </Tab>
 						  <Tab eventKey={7} title="BUNBUN" style={{color:'brown'}}>
 								<p> </p>
@@ -110,7 +110,7 @@ class LandingPage extends Component {
 
 						        <p><b>THE HUNTER</b></p>
 
-						        <p className="w3-text-grey" style={{textAlign:'left'}}> Artist | ENG/日本語 OK! Owner of Give Me Space.</p>
+						        <p className="w3-text-white" style={{textAlign:'left'}}> Artist | ENG/日本語 OK! Owner of Give Me Space.</p>
 						  </Tab>
 						  <Tab eventKey={8} title="SKY" style={{color:'#008080'}}>
 								<p> </p>
@@ -120,7 +120,7 @@ class LandingPage extends Component {
 
 						        <p><b>THE NOBLEWOMAN</b></p>
 
-						        <p className="w3-text-grey" style={{textAlign:'left'}}> Artist | ENG/日本語 OK! Owner of Give Me Space.</p>
+						        <p className="w3-text-white" style={{textAlign:'left'}}> Artist | ENG/日本語 OK! Owner of Give Me Space.</p>
 						  </Tab>
 						  <Tab eventKey={9} title="SHADOW" style={{color:'gray'}}>
 								<p> </p>
@@ -130,7 +130,7 @@ class LandingPage extends Component {
 
 						        <p><b>THE SPY</b></p>
 
-						        <p className="w3-text-grey" style={{textAlign:'left'}}> Feminist, Alcoholic, Social Entrepreneur, Community Celebratoire , Vampire Slayer. Battery low, gotta use my phone strategically now.</p>
+						        <p className="w3-text-white" style={{textAlign:'left'}}> Feminist, Alcoholic, Social Entrepreneur, Community Celebratoire , Vampire Slayer. Battery low, gotta use my phone strategically now.</p>
 						  </Tab>
 						</Tabs>
 
